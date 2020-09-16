fc789601dd4bcda559de00c98443c5e4a973292a
xxf <1731215302@qq.com>
2020-09-16 16:31:03 +0800

    mall商城

---
+++ src/main/java/com/mall/MallApplicationRun.java
@@ -0,0 +1,12 @@
+package com.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallApplicationRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallApplicationRun.class, args);
+    }
+}
